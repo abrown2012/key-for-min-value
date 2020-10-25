a3d88becab4529312a2aa9828f59323429a481ac
Agnieszka Brown <abrown2012@icloud.com>
2020-10-25 17:10:00 -0400

    commit tree fff1fe4d2c1b2e9bf649e5c8e2024bb48cdfa6e1

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,23 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+  
+    if name_hash.size == 0 
+        nil
+    else
+      new_value = 10000000
+      new_key = ()
+       name_hash.each do |key, value|
+        if value < new_value 
+            new_value = value  
+            new_key = key 
+        end 
+        
+      end 
+      return new_key
+      
+    end
+    
 end
